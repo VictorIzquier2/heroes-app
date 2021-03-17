b89770809e6bc5f82f080c41222260d81bc46722
VictorIzquier2 <victor.izquierdo1985@gmail.com>
2021-03-17 23:01:24 +0100

    2

--- src/components/heroes/HeroeList.js
+++ src/components/heroes/HeroeList.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher'
 import { HeroeCard } from './HeroeCard';
 
 export const HeroeList = ({publisher}) => {
 
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher])
+  //const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <div className='card-columns'>
+    <div className='card-columns animate__animated animate__fadeIn'>
       {
         heroes.map(heroe => (
           <HeroeCard 
--- src/components/heroes/HeroeScreen.js
+++ src/components/heroes/HeroeScreen.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, {useMemo} from 'react'
+import { Redirect, useParams } from 'react-router'
+import { getHeroeById } from '../../selectors/getHeroById';
 
-export const HeroeScreen = () => {
+export const HeroeScreen = ({history}) => {
+
+  const {heroeId} = useParams();
+  const heroe = useMemo(() => getHeroeById(heroeId), [heroeId])
+  //const heroe = getHeroeById(heroeId);
+  if(!heroe){
+    return <Redirect to='/'/>
+  }
+  const {
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters,
+  } = heroe;
+
+  const handleReturn = () => {
+    if(history.length <= 2){
+      history.push('/')
+    }
+    history.goBack();
+  }
+
   return (
-    <div>
-      <h1>HeroScreen</h1>   
+    <div className='row mt-5'>
+      <div className='col-4'>
+        <img
+          src={`../assets/heroes/${heroeId}.jpg`}
+          className='img-thumbnail animate__animated animate__fadeInLeft'
+          alt={superhero}
+        />
+      </div>
+      <div className='col-8'>
+        <h3>{superhero}</h3>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'><b>Alter ego: </b>{alter_ego}</li>
+          <li className='list-group-item'><b>Publisher: </b>{publisher}</li>
+          <li className='list-group-item'><b>First appearance: </b>{first_appearance}</li>
+        </ul>
+        <h5 className='mt-4'>Characters</h5>
+        <p>{characters}</p>
+
+        <button 
+          className='btn btn-outline-info'
+          onClick={handleReturn}
+        >Return</button>
+      </div>
     </div>
   )
 }
