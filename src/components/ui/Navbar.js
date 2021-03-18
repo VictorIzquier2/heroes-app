5c20fe533afece3f174d2ff529c3c43cccecbb08
VictorIzquier2 <victor.izquierdo1985@gmail.com>
2021-03-18 17:48:18 +0100

    login logout

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, NavLink, useHistory } from 'react-router-dom'
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const Navbar = () => {
+
+    const {user: {name}, dispatch} = useContext(AuthContext);
+    const history = useHistory();
+
+    const handleLogout = () => {
+        
+        history.replace('/login')
+        
+        dispatch({
+            type: types.logout
+        })
+    }
+
     return (
         <nav className="navbar px-5 navbar-expand-sm navbar-dark bg-dark">
             
             <Link 
                 className="navbar-brand" 
                 to="/"
-            >
-                Asociaciones
-            </Link>
+            >Asociaciones</Link>
 
             <div className="navbar-collapse">
                 <div className="navbar-nav">
@@ -32,19 +45,25 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
-                </div>
-            </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-                <ul className="navbar-nav ml-auto">
                     <NavLink 
                         activeClassName="active"
                         className="nav-item nav-link" 
                         exact
-                        to="/login"
+                        to="/search"
                     >
-                        Logout
+                        Search
                     </NavLink>
+                </div>
+            </div>
+            
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                <ul className="navbar-nav ml-auto">
+                    <span className='nav-item nav-link text-info'>{name}</span>
+                    <button 
+                        className="nav-item nav-link btn" 
+                        onClick={handleLogout}
+                    >Logout</button>
                 </ul>
             </div>
         </nav>
--- src/router/DashboardRoutes.js
+++ src/router/DashboardRoutes.js
@@ -5,16 +5,19 @@ import { Navbar } from '../components/ui/Navbar'
 import {MarvelScreen} from '../components/marvel/MarvelScreen';
 import {DcScreen} from '../components/dc/DcScreen';
 import {HeroeScreen} from '../components/heroes/HeroeScreen';
+import { SearchScreen } from '../components/search/SearchScreen';
 
 export const DashboardRoutes = () => {
+
   return (
     <Fragment>
-      <Navbar />
+      <Navbar/>
       <div className='container mt-4'>
         <Switch>
           <Route exact path='/marvel' component={MarvelScreen}/>
           <Route exact path='/dc' component={DcScreen}/>
           <Route exact path='/heroe/:heroeId' component={HeroeScreen}/>
+          <Route exact path='/search' component={SearchScreen}/>
 
           <Redirect to='/marvel' />
           
